07d797c5fea3ab331d027202e7684f9cd4e516b2
Md Tusar Imran <113105947+iamtusar1@users.noreply.github.com>
2023-08-22 12:19:46 +0600

    Add files via upload

---
+++ class-2/switch.php
@@ -0,0 +1,89 @@
+<?php
+
+    $Letter = "";
+
+    switch($Letter){
+        case "A":
+            echo " 'A' is a Vowel Letter ";
+            break;
+        case "B":
+            echo " 'B' is a Consonant Letter ";
+            break;
+        case "C":
+            echo " 'C' is a Consonant Letter ";
+            break;
+        case "D":
+            echo " 'D' is a Consonant Letter ";
+            break;
+        case "E":
+            echo " 'E' is a Vowel Letter ";
+            break;
+        case "F":
+            echo " 'F' is a Consonant Letter ";
+            break;
+        case "G":
+            echo " 'G' is a Consonant Letter ";
+            break;
+        case "H":
+            echo " 'H' is a Consonant Letter ";
+            break;
+        case "I":
+            echo " 'I' is a Vowel Letter ";
+            break;
+        case "J":
+            echo " 'J' is a Consonant Letter ";
+            break;
+        case "K":
+            echo " 'K' is a Consonant Letter ";
+            break;
+        case "L":
+            echo " 'L' is a Consonant Letter ";
+            break;
+        case "M":
+            echo " 'M' is a Consonant Letter ";
+            break;
+        case "N":
+            echo " 'N' is a Consonant Letter ";
+            break;
+        case "O":
+            echo " 'O' is a Vowel Letter ";
+            break;
+        case "P":
+            echo " 'P' is a Consonant Letter ";
+            break;
+        case "Q":
+            echo " 'Q' is a Consonant Letter ";
+            break;
+        case "R":
+            echo " 'R' is a Consonant Letter ";
+            break;
+        case "S":
+            echo " 'S' is a Consonant Letter ";
+            break;
+        case "T":
+            echo " 'T' is a Consonant Letter ";
+            break;
+        case "U":
+            echo " 'U' is a Vowel Letter ";
+            break;
+        case "V":
+            echo " 'V' is a Consonant Letter ";
+            break;
+        case "W":
+            echo " 'W' is a Consonant Letter ";
+            break;
+        case "X":
+            echo " 'X' is a Consonant Letter ";
+            break;
+        case "Y":
+            echo " 'Y' is a Consonant Letter ";
+            break;
+        case "Z":
+            echo " 'Z' is a Consonant Letter ";
+            break;
+        default:
+            echo " Plese! Write The Uppercase Letter ";
+       
+    }
+
+?>
